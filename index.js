610de00e7405226fc9b38f246f541ff7273b704b
Cris Lenard de Leon <wb-crislenar575527@antfin.com>
2019-07-19 17:58:00 +0800

    added sdk

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
+
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const
