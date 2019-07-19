7b2510b75144458d92308e77403260826bc74f76
Cris Lenard de Leon <wb-crislenar575527@antfin.com>
2019-07-19 10:29:51 +0800

    updated : sendtext v2

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
- 
+    let messaging_events = req.body.entry[0].messaging
+      for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+          let text = event.message.text
+          sendText(sender, "Text echo: " + text.substring(0, 100))
+        }
+      }
     let body = req.body;
   
     // Checks this is an event from a page subscription
@@ -25,15 +33,6 @@ app.post('/webhook', (req, res) => {
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
       });
-          let messaging_events = req.body.entry[0].messaging
-            for (let i = 0; i < messaging_events.length; i++) {
-              let event = messaging_events[i]
-              let sender = event.sender.id
-              if (event.message && event.message.text) {
-                let text = event.message.text
-                sendText(sender, "Text echo: " + text.substring(0, 100))
-              }
-            }
       // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');
     } else {
